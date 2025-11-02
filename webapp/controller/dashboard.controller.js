4f158c82ea486eeb62886c33569bcbc50b586fdf
Ritik Choubey <ritik.choubey@ltimindtree.com>
2025-11-02 18:41:32 +0000

    Products Listed and filter sorting and grouping added

--- webapp/controller/dashboard.controller.js
+++ webapp/controller/dashboard.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel"
+], (Controller,Filter,FilterOperator,JSONModel) => {
     "use strict";
 
     return Controller.extend("app01.controller.dashboard", {
@@ -11,9 +14,25 @@ sap.ui.define([
             var oUserModel = new sap.ui.model.json.JSONModel();
             this.getView().setModel(oUserModel,"oUserModel");
 
+            var oCurr = new sap.ui.model.json.JSONModel({
+                currency: "INR"
+            });
+            this.getView().setModel(oCurr,"curr")
+
+        },
+        onFilterInvoices(oEvent){
+            const aFilter = [];
+            const sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("Name",FilterOperator.Contains,sQuery));
+            }
+
+            //filter binding
+            const oList = this.byId("_IDGenList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         },
         _onObjectMatched(oEvent){
-            // comment Ritik
             let name = oEvent.getParameter("arguments").name;
             let pass = oEvent.getParameter("arguments").pass;
             let maskedPass = '*'.repeat(pass.length - 2) + pass.slice(-2);
@@ -27,6 +46,6 @@ sap.ui.define([
         onLogout() {
             var router = sap.ui.core.UIComponent.getRouterFor(this) // Learn it to get router object
             router.navTo("Routehome_view")
-        }
+        },
     });
 });
